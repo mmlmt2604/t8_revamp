71b56a86959a424fb9690874cb89dd9a19b976cb
Mike.Lin <mikelin9984@gmail.com>
2025-01-26 23:31:36 +0800

    backgreound.js initalized with warning/ weather data fetch

---
+++ entrypoints/background.ts
@@ -0,0 +1,71 @@
+export default defineBackground(() => {
+  console.log('Hello background!', { id: browser.runtime.id });
+  chrome.alarms.create('fetchData', { periodInMinutes: 0.1 });  
+  
+  chrome.alarms.onAlarm.addListener((alarm) => {  
+  if (alarm.name === 'fetchData') {  
+    console.log('Fetching data...');
+  fetchWeatherData();  
+  fetchWarningData();  
+  }  
+  }); 
+
+});
+
+
+let isUpdating = false; // Shared state to track update status  
+  
+function fetchWeatherData() {  
+if (isUpdating) return; // Skip if an update is already in progress  
+isUpdating = true;  
+  
+fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en') // Replace with actual API  
+.then(response => response.json())  
+.then(data => {  
+const weatherData = {  
+  generalSituation: data.generalSituation,  
+  tcInfo: data.tcInfo,  
+  fireDangerWarning: data.fireDangerWarning,  
+  forecastDesc: data.forecastDesc,  
+  outlook: data.outlook,  
+  updateTime: data.updateTime,  
+
+};  
+chrome.storage.sync.set({ weatherData });  
+// chrome.runtime.sendMessage({ action: 'updateWeather', data: weatherData });  
+})  
+.catch(error => {  
+console.error('Error fetching weather data:', error);  
+})  
+.finally(() => {  
+isUpdating = false; // Reset the flag after the update is complete  
+});  
+}  
+  
+function fetchWarningData() {  
+if (isUpdating) return; // Skip if an update is already in progress  
+isUpdating = true;  
+  
+fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=en') // Replace with actual API  
+.then(response => response.json())  
+.then(data => {  
+const warningData = data;  
+chrome.storage.sync.set({ warningData });  
+// chrome.runtime.sendMessage({ action: 'updateWarnings', data: warningData });  
+  
+// if (data) {  
+// chrome.notifications.create({  
+// type: 'basic',  
+// iconUrl: 'icon.png',  
+// title: 'Cyclone Warning',  
+// message: `Warning Level: ${data.level}`,  
+// });  
+// }  
+})  
+.catch(error => {  
+console.error('Error fetching warning data:', error);  
+})  
+.finally(() => {  
+isUpdating = false; // Reset the flag after the update is complete  
+});  
+}  
